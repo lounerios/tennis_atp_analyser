b24d3c01738d2e374537c235d9bd0c451ed0bc26
simopoul <lucas.simopoulos@nokia.com>
2018-12-11 14:52:13 +0000

    Add indexes to the schema.

--- atp_database_schema_v1_1.sql
+++ atp_database_schema_v1_1.sql
@@ -12,3 +12,8 @@ CREATE TABLE atp_ranking(
         player_id INTEGER NOT NULL,
         points INTEGER NOT NULl
     );
+CREATE INDEX plr_country on player (country);
+CREATE INDEX plr_status on player (status);
+CREATE INDEX rnk_date on atp_ranking(date);
+CREATE INDEX rnk_number on atp_ranking(number);
+CREATE INDEX rnk_player_id on atp_ranking(player_id);
